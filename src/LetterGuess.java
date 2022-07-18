eebb9507be7c4decfbfb0187e3007619c45720a6
Holger Mueller <9jholger@gmail.com>
2022-07-18 12:01:47 -0400

    basic gameplay works

--- src/LetterGuess.java
+++ src/LetterGuess.java
@@ -7,12 +7,25 @@ public class LetterGuess {
     Random rand = new Random();
     String userChoice = "";
     int guessesLeft = 3;
-    int guessesTaken = 0;
     String[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
         "T", "U",
         "V", "W", "X", "Y", "Z" };
     int index = rand.nextInt(letters.length);
 
-    System.out.println(letters[index]);
+    System.out.println("Welcome to the letter guess game!");
+    System.out.println("You have " + guessesLeft + " chances to win.");
+    System.out.println("Please choose one letter from the alphabet:");
+    userChoice = sc.nextLine().toUpperCase();
+
+    if (userChoice.equals(letters[index])) {
+      System.out.println("Congratualations! You have won!");
+    } else if (!userChoice.equals(letters[index])) {
+      System.out.println("Sorry, that is not the correct letter.");
+      guessesLeft -= 1;
+      System.out.println("You have " + guessesLeft + " remaining. Please choose again:");
+    } else if (guessesLeft == 0) {
+      System.out.println("Sorry, the correct letter was " + letters[index] + ". You lose!");
+    }
+
   }
 }
